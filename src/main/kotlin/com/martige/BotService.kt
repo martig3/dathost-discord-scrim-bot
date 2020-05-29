cd2f48a32c309e4e2108632d086c5c1a235eda03
MartinWienc <32913968+MartinWienc@users.noreply.github.com>
2020-05-28 23:57:49 -0400

    increased length of delay on upload demos coroutine

--- src/main/kotlin/com/martige/BotService.kt
+++ src/main/kotlin/com/martige/BotService.kt
@@ -386,7 +386,7 @@ class BotService(props: Properties, private var jda: JDA) {
                     clearTime.set(Calendar.SECOND, 0)
                 }
                 val msBetween = clearTime.time.toInstant().toEpochMilli() - currentDate.toInstant().toEpochMilli()
-                if (msBetween > 0) {
+                if (msBetween > 1) {
                     log.info("Autoclearing in ${msBetween}ms")
                     delay(msBetween)
                     val channel = jda.getTextChannelById(discordTextChannelId) ?: return@launch
